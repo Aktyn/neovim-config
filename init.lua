53f5129876719501538bbb59220733c55b2991db
Aktyn <aktyn3@gmail.com>
2025-06-02 00:08:22 +0200

    Folds save management

--- init.lua
+++ init.lua
@@ -1,2 +1,25 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_augroup("remember_folds", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  group = "remember_folds",
+  pattern = "*",
+  callback = function()
+  -- Only for normal files
+  if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+    vim.cmd("mkview")
+    end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = "remember_folds",
+  pattern = "*",
+  callback = function()
+  if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+    vim.cmd("silent! loadview")
+    end
+    end,
+})
